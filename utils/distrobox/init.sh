0dbd133d48a3359364b2597f1d9a2ae514dea4ec
Tobias Bräutigam <tbraeutigam@gmail.com>
2026-07-15 16:20:14 +0000

    chore: adapt distrobox for new base container
    knxd has no sudo support, also add an option to install custom packages

--- utils/distrobox/init.sh
+++ utils/distrobox/init.sh
@@ -12,19 +12,17 @@ if [ ! -f /root/.first_run_complete ]; then
     sudo rm /var/run/apache2/* || true
     
     # # apply customizations for cometvisu
-    NO_ENTRYPOINT_APACHE=true NO_ENTRYPOINT_KNXD=true sudo -E /usr/local/bin/cometvisu-entrypoint > /root/init.log 2>&1
+    NO_ENTRYPOINT_APACHE=true sudo -E /usr/local/bin/cometvisu-entrypoint > /root/init.log 2>&1
     # override healthcheck to always succeed
     echo "#!/bin/sh" > /usr/local/bin/healthcheck
     
     # change apache to listen on 8080 instead of 80 to avoid conflicts with host system
     sudo sed -i "s/^Listen 80$/Listen 8080/" /etc/apache2/ports.conf
     sudo sed -i "s/^<VirtualHost \*:80>$/<VirtualHost *:8080>/" /etc/apache2/sites-enabled/000-default.conf   
-    
-    sudo cp /home/cv/utils/distrobox/knxd.socket /usr/lib/systemd/system/knxd.socket
 
     sudo systemctl enable apache2
-    sudo systemctl enable knxd    
 
+    # create symlinks for distrobox-host-exec to allow running docker and kde-open
     ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker
     ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/kde-open
     # install python dependencies
